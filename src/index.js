0ef6a90dc7a35e6b6f0bb2bd4463f8456635f81f
stephane-vanraes <stephane.vanraes@gmail.com>
2020-07-20 19:15:58 +0200

    v0.2.1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import Notification from './Notifications.svelte'
 import{ notifications } from './Notifications.svelte'
+import Carousel from './Carousel.svelte'
 
 export { 
+    Carousel,
     Notification,
     notifications
 }
 
 export const Renderless = {
+    Carousel,
     Notification,
     notifications
 }
