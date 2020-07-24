def3d5271bc97af1565f843beb4c67c5b2d31f90
Stephane Vanraes <stephane.vanraes@fjordline.com>
2020-07-24 07:50:45 +0200

    changed title to payload in tabcontrol

--- src/TabControlItem.svelte
+++ src/TabControlItem.svelte
@@ -5,22 +5,24 @@
 	
 	export let active = false
 	export let disabled = false
-	export let title	
+	export let id
+	export let payload	
 	
-	const select = () => setTab(title)
+	const select = () => setTab(payload)
 	
 	onMount(() => _tabs.update(t => [...t, {		
 			active,
 			disabled,
+			id,
 			select,
-			title
+			payload
 		}])
 	)
 	
-	$: tab = $_tabs.find(t => title === t.title)
+	$: tab = $_tabs.find(t => id === t.id)
 	$: _active = tab && tab.active
-	$: _tabs.update(t1 => t1.map(t2 => t2.title === title ? ({ ...t2, disabled }) : t2 ))
-	$: _tabs.update(t1 => t1.map(t2 => t2.title === title ? ({ ...t2, active }) : t2 ))
+	$: _tabs.update(t1 => t1.map(t2 => t2.id === id ? ({ ...t2, disabled }) : t2 ))
+	$: _tabs.update(t1 => t1.map(t2 => t2.id === id ? ({ ...t2, active }) : t2 ))
 </script>
 
 {#if _active}
