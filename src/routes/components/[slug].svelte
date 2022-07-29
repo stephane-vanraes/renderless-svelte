ec0ef0ddeb97f048c60cad7163e321dc65a35885
stephane-vanraes <stephane.vanraes@gmail.com>
2022-07-29 12:44:29 +0200

    random colours everywhere

--- src/routes/components/[slug].svelte
+++ src/routes/components/[slug].svelte
@@ -18,15 +18,44 @@
 </script>
 
 <TabControl>
-	<div slot="tabs" let:tabs>
+	<div class="tabs" slot="tabs" let:tabs>
 		{#each tabs as tab}
-			<button on:click={tab.select}>{tab.payload}</button>
+			<button on:click={tab.select} class:active={tab.active}>{tab.payload}</button>
 		{/each}
 	</div>
-	<TabControlItem active payload="Documentation">
-		<Article {markup} />
-	</TabControlItem>
-	<TabControlItem payload="Demos">
-		<Demos {demos} />
-	</TabControlItem>
+	<div class="content">
+		<TabControlItem active payload="Documentation">
+			<Article {markup} />
+		</TabControlItem>
+		<TabControlItem payload="Demos">
+			<Demos {demos} />
+		</TabControlItem>
+	</div>
 </TabControl>
+
+<style>
+	button {
+		background-color: transparent;
+		border-color: var(--gray-600);
+		border-style: dashed;
+		border-width: 1px 1px 0 0;
+		padding: 0.25rem 1rem;
+	}
+	button:first-child {
+		border-inline-start-width: 1px;
+	}
+	button.active {
+		background-color: var(--primary-600);
+		border-style: solid;
+	}
+	button:is(:active, :focus, :hover) {
+		background-color: var(--primary-200);
+		border-style: solid;
+	}
+
+	.content {
+		background-color: var(--gray-800);
+		border: 1px solid black;
+		padding: 1rem;
+	}
+</style>
--- src/site/components/Article.svelte
+++ src/site/components/Article.svelte
@@ -5,3 +5,45 @@
 <article>
 	{@html markup}
 </article>
+
+<style>
+	article {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+
+	article > :global(h1) {
+		font-size: 2rem;
+		font-weight: 600;
+	}
+
+	article > :global(h2) {
+		font-weight: 600;
+	}
+
+	article > :global(pre) {
+		background-color: var(--gray-600);
+		border-inline-start: 1rem solid var(--secondary-400);
+		padding: 0.5rem;
+	}
+
+	article > :global(table) {
+		border-collapse: collapse;
+		margin: 0 1rem;
+	}
+
+	article > :global(table th) {
+		border-bottom: 1px solid black;
+		font-weight: 600;
+		padding: 0.25rem;
+		text-align: start;
+	}
+
+	article > :global(table td) {
+		padding: 0.25rem;
+	}
+	article > :global(table tr + tr) {
+		border-top: 1px solid black;
+	}
+</style>
